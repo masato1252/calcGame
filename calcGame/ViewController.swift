dddc4e9964023e2487ad27cbd8303d11436bddee
松浦 雅人 <masato@masato-matsuuras-macbook-air.local>
2017-01-12 02:08:16 +0900

    Initial Commit

---
+++ calcGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calcGame
+//
+//  Created by 松浦 雅人 on 2017/01/12.
+//  Copyright © 2017年 松浦 雅人. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
